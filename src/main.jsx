5f62adb9ba9300090d4259debcb1d1af6231e002
spahdhkd-sketch <spahdhkd@gmail.com>
2026-05-07 19:05:01 +0900

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,7 @@
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import './index.css';   // ← 이 줄 추가
 import App from './App';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
